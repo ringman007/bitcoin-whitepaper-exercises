ed914ea620d30b1cd38db6241d69160a5ed9bdd4
ringman007 <51226889+ringman007@users.noreply.github.com>
2021-09-10 08:40:27 +0200

    Update hashing.js

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -27,9 +27,46 @@ Blockchain.blocks.push({
 	timestamp: Date.now(),
 });
 
+// `index`, `prevHash`, `data`, and `timestamp`
+// first trial
+// const createBlock = (_data) => ({
+//     index: Blockchain.blocks.length-1,
+//     prevHash: Blockchain.blocks,
+//     data: _data,
+//     timestamp: Date.now(),
+// })
+
+// second trial
+// function createBlock(_data) {
+//     let block = {
+//         index: Blockchain.blocks.length,
+//         prevHash: Blockchain.blocks[Blockchain.blocks.length-1].hash,
+//         data: _data,
+//         timestamp: Date.now(),
+//     };
+
+// third trial
+const createBlock = (_data) => {
+    let block = {
+        index: Blockchain.blocks.length,
+        prevHash: Blockchain.blocks[Blockchain.blocks.length-1].hash,
+        data: _data,
+        timestamp: Date.now(),
+    }
+
+    block.hash = blockHash(block);
+    Blockchain.blocks.push(block)
+    console.log(Blockchain.blocks);
+    return block;
+}
+
 // TODO: insert each line into blockchain
+// poem.forEach(createBlock());
 // for (let line of poem) {
+// 	createBlock(line)
 // }
+
+poem.forEach(createBlock);
 
 // console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
 
@@ -39,5 +76,44 @@ Blockchain.blocks.push({
 function blockHash(bl) {
 	return crypto.createHash("sha256").update(
 		// TODO: use block data to calculate hash
+        `${bl.index}; ${bl.prevHash}; ${bl.data}; ${bl.timestamp}`
 	).digest("hex");
 }
+
+
+function verfyChain() {
+
+    if (verifyBlock() === true) {
+        console.log("Blockchain seems to be valid!!!")
+        return 
+    } else {
+        console.log("Blockchain not valid")
+    }
+}
+
+function verifyBlock() {
+    let prevBlock = Blockchain.blocks[Blockchain.blocks.length - 2];
+    let nowBlock = Blockchain.blocks[Blockchain.blocks.length - 1 ];
+    let genBlock = Blockchain.blocks[0];
+    let bl = nowBlock;
+
+    if (
+        genBlock.hash === "000000"
+        &&nowBlock.data !== 'undefined' 
+        && nowBlock.prevHash !== 'undefined' 
+        && nowBlock.index >= 0
+        && prevBlock.hash === nowBlock.prevHash
+        && nowBlock.hash === blockHash(bl)
+    ) {
+        // console.log("nice");
+        return true;
+    } else {
+        // console.log("not so nice");
+        return false;
+    }   
+}
+
+Blockchain.blocks.forEach(verifyBlock);
+verfyChain();
+
+// Did I really check if the hashes match?
